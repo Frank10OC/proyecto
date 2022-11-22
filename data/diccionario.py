8f1ae54dd9087aafd5a25d002c2e852666917222
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2022-11-21 19:36:03 -0500

    Add files via upload

---
+++ data/diccionario.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+@st.experimental_memo
+def load_data():
+    return pd.read_csv("Indices_Soberanos_Diccionario.csv")
+st.checkbox("Use container width", value=False, key="use_container_width")
+
+df = load_data()
+st.dataframe(df, use_container_width=st.session_state.use_container_width)
