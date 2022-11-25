b6debb2a68e8b1e351deede418dbecb1305d187f
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2022-11-25 08:55:09 -0500

    Update data.py

--- data/data.py
+++ data/data.py
@@ -8,3 +8,7 @@ st.checkbox("Use container width", value=False, key="use_container_width")
 
 df = load_data()
 st.dataframe(df, use_container_width=st.session_state.use_container_width)
+pie_chart = px.pie(df_personas2, #tomo el dataframe2
+                   title = 'Total No. of Participants', #El titulo
+                   values = 'EDAD PERSONA ENCUESTADA',##columna
+                   names = 'EPS') ## para verlo por EPS --> Colores
