db40f68c8eef705294fbb5b3a3f6d6a510493ece
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2022-11-27 23:51:07 -0500

    Update data.py

--- data/data.py
+++ data/data.py
@@ -22,8 +22,6 @@ def load_data():
     url="https://raw.githubusercontent.com/Frank10OC/proyecto/main/data/indices_soberanos.csv"
     return pd.read_csv(url)
 st.checkbox("Use container width", value=False, key="use_container_width")
+c= load_data()
+st.line_chart(c)
 
-df = load_data()
-st.dataframe(df, use_container_width=st.session_state.use_container_width)
-pie_grf = px.pie(df, title = 'Total No. of Participants', values = 'EDAD PERSONA ENCUESTADA',names = 'EPS')
-st.plotly_chart(pie_grf)
