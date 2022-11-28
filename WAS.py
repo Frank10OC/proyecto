270e791b604e6a4eab33bc2afaf52bac8008fbd9
personafujoshi <118547837+personafujoshi@users.noreply.github.com>
2022-11-27 20:03:30 -0500

    Update WAS.py

--- WAS.py
+++ WAS.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import pandas as pd
 
-data=pd.read_csv("indices_soberanos.csv",sep=';',nrows=1000000, parse_dates=["INDICE_NOMINAL","INDICE_REAL"])
+url="https://raw.githubusercontent.com/Frank10OC/proyecto/main/data/indices_soberanos.csv"
+data=pd.read_csv("indices_soberanos.csv",sep=";",nrows=1000000, parse_dates=["INDICE_NOMINAL","INDICE_REAL"]
 st.dataframe(data.head(20))
 ind_nom=data["FECHA"]
 st.line_chart(ind_nom)
