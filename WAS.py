0a2b321a2236738d109475c3250827c154a6667f
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2022-11-27 20:15:29 -0500

    Update WAS.py

--- WAS.py
+++ WAS.py
@@ -3,7 +3,7 @@ import pandas as pd
 import gdown
 
 url="https://raw.githubusercontent.com/Frank10OC/proyecto/main/data/indices_soberanos.csv"
-data=pd.read_csv(url,sep=";",nrows=1000000, parse_dates=["INDICE_NOMINAL","INDICE_REAL"]
+data=pd.read_csv(url,sep=";",nrows=1000000, parse_dates=["INDICE_NOMINAL","INDICE_REAL"])
 st.dataframe(data.head(20))
 ind_nom=data["FECHA"]
 st.line_chart(ind_nom)
