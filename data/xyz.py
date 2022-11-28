a84f00a74de1592160f57b43e864154964065854
Jhorchql <117998365+Jhorchql@users.noreply.github.com>
2022-11-27 20:27:06 -0500

    Update xyz.py

--- data/xyz.py
+++ data/xyz.py
@@ -5,7 +5,7 @@ import gdown
 def download_data():
   url="https://drive.google.com/file/d/1nZkoRX8956K9lybIb-ARHZ3Jk4QLiDw0/view?usp=share_link"
   output="data.csv"
-  gdown.download(url,output,quiet=false)
+  gdown.download(url,output,quiet=False)
   
 download_data()
 data=pd.read_csv("data.csv",sep=";",nrows=1000000,parse_dates=["INDICE_NOMINAL","INDICE_REAL"])
