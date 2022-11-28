14a82d8b327090f438bdb6ebc4126c27664d6a28
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2022-11-27 20:17:23 -0500

    Update WAS.py

--- WAS.py
+++ WAS.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import gdown
 
 url="https://raw.githubusercontent.com/Frank10OC/proyecto/main/data/indices_soberanos.csv"
 data=pd.read_csv(url,sep=";",nrows=1000000, parse_dates=["INDICE_NOMINAL","INDICE_REAL"])
