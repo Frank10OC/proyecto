6c52e7f02970b36677f327105a3a9217fdd6207b
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2022-11-27 23:06:37 -0500

    Update data.py

--- data/data.py
+++ data/data.py
@@ -1,6 +1,22 @@
 import pandas as pd
 import streamlit as st
 @st.experimental_memo
+#título
+st . título ( 'Índices Soberanos 2010 - 2022' )
+st . subtítulo ( "Miembros del equipo" )
+st . descuento ( """
+-Frank Orozco Chupos
+-Ninoska 
+-Yasmin
+-Jorch
+""" )
+st . descuento ( """
+---
+La información contenida en esta página web permite acceder al Dataset “Índices Soberanos 2010 - 2022”
+Elaborado por el Ministerio de Economía y Finanzas (MINSA) del Perú. En la cual esta registrado los diferentes 
+Fuente de datos: (https://www.datosabiertos.gob.pe/dataset/%C3%ADndices-soberanos-2010-2022)
+---
+""" )
 def load_data():
     url="https://raw.githubusercontent.com/Frank10OC/proyecto/main/data/indices_soberanos.csv"
     return pd.read_csv(url)
