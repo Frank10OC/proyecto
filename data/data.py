8ffe02364f42f7321279eb4018187435ab74f6cf
Jhorchql <117998365+Jhorchql@users.noreply.github.com>
2022-11-28 19:35:00 -0500

    Update data.py

--- data/data.py
+++ data/data.py
@@ -6,10 +6,10 @@ import numpy as np
 st.title('Índices Soberanos 2010 - 2022')
 st.subheader("Miembros del equipo")
 st.markdown("""
-- Ninosca
-- Frank
-- Jorch
-- Yasmin
+- Palacios Ninahuanca, Ninoska
+- Orozco Chupos, Frank
+- Quispe Laura, Jhorch
+- Parillo Sanchez, Yassmin Diana
 """)
 st.markdown("""
 ---
@@ -19,7 +19,28 @@ Fuente de datos: (https://www.datosabiertos.gob.pe/dataset/%C3%ADndices-soberano
 ---
 """)
 def load_data():
-    url="https://raw.githubusercontent.com/Frank10OC/proyecto/main/data/indices_soberanos.csv"
+    url="https://raw.githubusercontent.com/Frank10OC/proyecto/main/indices_soberanos%20(1).csv"
     return pd.read_csv(url)
-c= load_data()
-st.line_chart(c)
+st.checkbox("Use container width", value=False, key="use_container_width")
+
+df = load_data()
+st.dataframe(df, use_container_width=st.session_state.use_container_width)
+url="https://raw.githubusercontent.com/Frank10OC/proyecto/main/indices_soberanos%20(1).csv"
+c=pd.read_csv(url)
+#comparación del Índice nominal e Índice real 
+df = c.drop(columns = ["FECHA","INDICE_NOMINAL","INDICE_REAL"])
+st.dataframe(df, use_container_width=st.session_state.use_container_width)
+st.line_chart(df)
+#"INDICE_NOMINAL","RENT_ANUAL_IN"
+i1= c.drop(columns = ["FECHA","INDICE_REAL","RENT_ANUAL_IR"])
+st.dataframe(i1, use_container_width=st.session_state.use_container_width)
+#"INDICE_NOMINAL","RENT_ANUAL_IN"
+i2= c.drop(columns = ["FECHA","INDICE_NOMINAL","RENT_ANUAL_IN"])
+st.dataframe(i2, use_container_width=st.session_state.use_container_width)
+
+df1= c
+set_fecha= np.sort(df1["FECHA"].dropna().unique())
+#Seleccion del departamento
+opcion_fecha = st.selectbox('Selecciona una fecha', set_fecha)
+df_fecha = df1[df1["FECHA"] == opcion_fecha]
+num_filas = len(df_fecha.axes[0])
